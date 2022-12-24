4e79e6c0690e8206d5fe154ab1321f7444977ee3
Meenujiwani <114684403+Meenujiwani@users.noreply.github.com>
2022-12-24 23:02:17 +0530

    Update bye.java

--- bye.java
+++ bye.java
@@ -3,6 +3,6 @@ class Simple{
     public static void main (String args[])
     {
    
-      System.out.println("hello this is my first java code from bye batch");
+      System.out.println("hello this is my first java code from bye batch 27 and i also doing automatically ci part as well");
        }
    }
